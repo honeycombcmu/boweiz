053510b65ae82d32ca881d63af983da4049071b9
boweiz <boweiz@andrew.cmu.edu>
2016-03-28 03:45:08 -0400

    Added files via upload

---
+++ dashboard/public/assets/javascripts/gridData.js
@@ -0,0 +1,625 @@
+﻿
+
+var gridData =
+[
+    {
+        "id": 0,
+        "guid": "56e05974-4184-47c6-b035-a7139ea80a5f",
+        "isActive": false,
+        "balance": 2013,
+        "picture": "//placehold.it/32x32",
+        "age": 39,
+        "name": "Sue Sharpe",
+        "gender": "female",
+        "company": "Mitroc",
+        "email": "suesharpe@mitroc.com",
+        "phone": "+1 (951) 414-3324",
+        "address": "933 Herkimer Court, Greensburg, Iowa, 2881",
+        "registered": "1990-11-05T03:51:02 -02:00",
+        "latitude": -42.444761,
+        "longitude": 125.590428,
+        "tags": [
+            "qui",
+            "qui",
+            "fugiat",
+            "anim",
+            "sunt",
+            "reprehenderit",
+            "non"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Monique Henson"
+            },
+            {
+                "id": 1,
+                "name": "Mallory Fleming"
+            },
+            {
+                "id": 2,
+                "name": "Madeline Romero"
+            },
+            {
+                "id": 3,
+                "name": "Patsy Cortez"
+            },
+            {
+                "id": 4,
+                "name": "Verna Miles"
+            },
+            {
+                "id": 5,
+                "name": "Theresa Mosley"
+            },
+            {
+                "id": 6,
+                "name": "Georgia Hooper"
+            }
+        ]
+    },
+    {
+        "id": 1,
+        "guid": "757ba20d-c025-4303-89e5-955fb4be4361",
+        "isActive": true,
+        "balance": 1363,
+        "picture": "//placehold.it/32x32",
+        "age": 34,
+        "name": "Nieves Hubbard",
+        "gender": "male",
+        "company": "Syntac",
+        "email": "nieveshubbard@syntac.com",
+        "phone": "+1 (947) 476-2875",
+        "address": "388 Oxford Street, Englevale, Connecticut, 4945",
+        "registered": "2012-09-26T07:10:49 -03:00",
+        "latitude": -86.998205,
+        "longitude": 120.74761,
+        "tags": [
+            "consequat",
+            "nisi",
+            "quis",
+            "aliquip",
+            "proident",
+            "reprehenderit",
+            "voluptate"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Ophelia Spears"
+            },
+            {
+                "id": 1,
+                "name": "Padilla Trujillo"
+            },
+            {
+                "id": 2,
+                "name": "Adkins Wiley"
+            },
+            {
+                "id": 3,
+                "name": "Jenifer Lawson"
+            },
+            {
+                "id": 4,
+                "name": "Gardner Chase"
+            },
+            {
+                "id": 5,
+                "name": "Johnnie Bond"
+            },
+            {
+                "id": 6,
+                "name": "Mcdaniel Wells"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "guid": "1fc78d11-83e4-4179-b795-241fd5529205",
+        "isActive": false,
+        "balance": 1669,
+        "picture": "//placehold.it/32x32",
+        "age": 30,
+        "name": "Anastasia Underwood",
+        "gender": "female",
+        "company": "Gallaxia",
+        "email": "anastasiaunderwood@gallaxia.com",
+        "phone": "+1 (840) 409-3171",
+        "address": "382 Clark Street, Conestoga, New Hampshire, 2046",
+        "registered": "1997-12-21T01:33:32 -02:00",
+        "latitude": 48.923362,
+        "longitude": 175.985131,
+        "tags": [
+            "consectetur",
+            "anim",
+            "mollit",
+            "sint",
+            "ullamco",
+            "voluptate",
+            "quis"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Ayala Fulton"
+            },
+            {
+                "id": 1,
+                "name": "Beard Vaughan"
+            },
+            {
+                "id": 2,
+                "name": "Stafford Richards"
+            },
+            {
+                "id": 3,
+                "name": "Buchanan Estes"
+            },
+            {
+                "id": 4,
+                "name": "Yvonne Dudley"
+            },
+            {
+                "id": 5,
+                "name": "Jewell Montoya"
+            },
+            {
+                "id": 6,
+                "name": "Pollard Grant"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "guid": "0e870730-176d-47ec-8167-ed5cbb3d81a1",
+        "isActive": true,
+        "balance": 2787,
+        "picture": "//placehold.it/32x32",
+        "age": 21,
+        "name": "Maxine Haley",
+        "gender": "female",
+        "company": "Songbird",
+        "email": "maxinehaley@songbird.com",
+        "phone": "+1 (999) 509-2989",
+        "address": "731 Perry Place, Waumandee, Massachusetts, 8050",
+        "registered": "2002-04-14T23:15:48 -03:00",
+        "latitude": -58.70704,
+        "longitude": 89.342452,
+        "tags": [
+            "ullamco",
+            "laborum",
+            "exercitation",
+            "occaecat",
+            "amet",
+            "labore",
+            "commodo"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Estes Farley"
+            },
+            {
+                "id": 1,
+                "name": "Whitney Mayer"
+            },
+            {
+                "id": 2,
+                "name": "Castro Russo"
+            },
+            {
+                "id": 3,
+                "name": "Burton Velez"
+            },
+            {
+                "id": 4,
+                "name": "Townsend Franklin"
+            },
+            {
+                "id": 5,
+                "name": "Jeannine Roy"
+            },
+            {
+                "id": 6,
+                "name": "Greene May"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "guid": "bba93bab-c3d2-4cfa-b583-49a668c38edc",
+        "isActive": false,
+        "balance": 2943,
+        "picture": "//placehold.it/32x32",
+        "age": 36,
+        "name": "Bennett Alvarez",
+        "gender": "male",
+        "company": "Marvane",
+        "email": "bennettalvarez@marvane.com",
+        "phone": "+1 (965) 404-2466",
+        "address": "322 Story Street, Sunwest, Georgia, 4354",
+        "registered": "1997-04-26T06:13:27 -03:00",
+        "latitude": 4.087929,
+        "longitude": 151.328011,
+        "tags": [
+            "minim",
+            "aliquip",
+            "dolore",
+            "nostrud",
+            "ex",
+            "anim",
+            "fugiat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Mcgowan Matthews"
+            },
+            {
+                "id": 1,
+                "name": "Wagner Watkins"
+            },
+            {
+                "id": 2,
+                "name": "Rosie Chang"
+            },
+            {
+                "id": 3,
+                "name": "Carissa Walls"
+            },
+            {
+                "id": 4,
+                "name": "Sullivan Barry"
+            },
+            {
+                "id": 5,
+                "name": "Tate Solomon"
+            },
+            {
+                "id": 6,
+                "name": "Rosalyn Howard"
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "guid": "4ffce452-1da1-4e81-adf2-0cbf83e90159",
+        "isActive": true,
+        "balance": 2678,
+        "picture": "//placehold.it/32x32",
+        "age": 29,
+        "name": "Myrna Ellison",
+        "gender": "female",
+        "company": "Zoxy",
+        "email": "myrnaellison@zoxy.com",
+        "phone": "+1 (938) 461-3741",
+        "address": "418 Borinquen Pl, Islandia, South Dakota, 4382",
+        "registered": "1999-11-29T18:46:51 -02:00",
+        "latitude": -1.315113,
+        "longitude": -9.743829,
+        "tags": [
+            "tempor",
+            "Lorem",
+            "aliqua",
+            "ea",
+            "eu",
+            "aliquip",
+            "cupidatat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Gallegos Decker"
+            },
+            {
+                "id": 1,
+                "name": "Byers Harrison"
+            },
+            {
+                "id": 2,
+                "name": "Doris Lindsey"
+            },
+            {
+                "id": 3,
+                "name": "Garner Silva"
+            },
+            {
+                "id": 4,
+                "name": "Rowena Emerson"
+            },
+            {
+                "id": 5,
+                "name": "Nixon Contreras"
+            },
+            {
+                "id": 6,
+                "name": "Whitaker Mercer"
+            }
+        ]
+    },
+    {
+        "id": 6,
+        "guid": "b7948be2-be93-4e84-a68e-94b22598b410",
+        "isActive": false,
+        "balance": 1148,
+        "picture": "//placehold.it/32x32",
+        "age": 37,
+        "name": "Karyn Day",
+        "gender": "female",
+        "company": "Apex",
+        "email": "karynday@apex.com",
+        "phone": "+1 (828) 455-2397",
+        "address": "847 Otsego Street, Beaverdale, Montana, 6987",
+        "registered": "1991-04-07T16:23:34 -03:00",
+        "latitude": -76.943928,
+        "longitude": 81.812895,
+        "tags": [
+            "voluptate",
+            "cillum",
+            "deserunt",
+            "reprehenderit",
+            "nisi",
+            "pariatur",
+            "sint"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Nunez Madden"
+            },
+            {
+                "id": 1,
+                "name": "Mcguire Pope"
+            },
+            {
+                "id": 2,
+                "name": "Norton Peck"
+            },
+            {
+                "id": 3,
+                "name": "Tia Hester"
+            },
+            {
+                "id": 4,
+                "name": "Neva Chen"
+            },
+            {
+                "id": 5,
+                "name": "Imogene Myers"
+            },
+            {
+                "id": 6,
+                "name": "Shepard Gibbs"
+            }
+        ]
+    },
+    {
+        "id": 7,
+        "guid": "445de5ba-f953-42dd-9620-38f6a18a2952",
+        "isActive": true,
+        "balance": 1132,
+        "picture": "//placehold.it/32x32",
+        "age": 24,
+        "name": "Carolyn Bernard",
+        "gender": "female",
+        "company": "Interfind",
+        "email": "carolynbernard@interfind.com",
+        "phone": "+1 (816) 427-2550",
+        "address": "190 Lois Avenue, Leroy, Maine, 1139",
+        "registered": "2000-05-02T13:30:58 -03:00",
+        "latitude": -35.896506,
+        "longitude": -115.569305,
+        "tags": [
+            "enim",
+            "dolore",
+            "magna",
+            "aliqua",
+            "cillum",
+            "deserunt",
+            "cupidatat"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Trisha Shepherd"
+            },
+            {
+                "id": 1,
+                "name": "Alisa Duffy"
+            },
+            {
+                "id": 2,
+                "name": "Marquez Sanders"
+            },
+            {
+                "id": 3,
+                "name": "Roberts Miranda"
+            },
+            {
+                "id": 4,
+                "name": "Marsh Padilla"
+            },
+            {
+                "id": 5,
+                "name": "Mcclain Martin"
+            },
+            {
+                "id": 6,
+                "name": "Lorrie Chaney"
+            }
+        ]
+    },
+    {
+        "id": 8,
+        "guid": "d5bc6510-457a-48c6-a2be-898201000cde",
+        "isActive": false,
+        "balance": 3140,
+        "picture": "//placehold.it/32x32",
+        "age": 35,
+        "name": "Tamara Long",
+        "gender": "female",
+        "company": "Emtrak",
+        "email": "tamaralong@emtrak.com",
+        "phone": "+1 (800) 559-3292",
+        "address": "442 Orange Street, Drytown, West Virginia, 6610",
+        "registered": "2001-06-27T21:28:47 -03:00",
+        "latitude": -77.403482,
+        "longitude": -106.617193,
+        "tags": [
+            "Lorem",
+            "exercitation",
+            "aute",
+            "nulla",
+            "nulla",
+            "id",
+            "qui"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Sparks Langley"
+            },
+            {
+                "id": 1,
+                "name": "Carpenter Boyer"
+            },
+            {
+                "id": 2,
+                "name": "Jordan Maldonado"
+            },
+            {
+                "id": 3,
+                "name": "Marie Richard"
+            },
+            {
+                "id": 4,
+                "name": "Dianna Cleveland"
+            },
+            {
+                "id": 5,
+                "name": "Wendy Nolan"
+            },
+            {
+                "id": 6,
+                "name": "Josefa Branch"
+            }
+        ]
+    },
+    {
+        "id": 9,
+        "guid": "5ddce43f-61ee-4ad0-8f07-c0ad8317332c",
+        "isActive": true,
+        "balance": 1870,
+        "picture": "//placehold.it/32x32",
+        "age": 26,
+        "name": "Farley Booth",
+        "gender": "male",
+        "company": "Applidec",
+        "email": "farleybooth@applidec.com",
+        "phone": "+1 (838) 559-3687",
+        "address": "769 Hart Street, Websterville, Idaho, 9878",
+        "registered": "2000-10-30T13:11:26 -02:00",
+        "latitude": 3.388551,
+        "longitude": -122.049386,
+        "tags": [
+            "eiusmod",
+            "laborum",
+            "quis",
+            "consectetur",
+            "cillum",
+            "velit",
+            "eu"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Thelma Burt"
+            },
+            {
+                "id": 1,
+                "name": "Chan Mueller"
+            },
+            {
+                "id": 2,
+                "name": "May Johns"
+            },
+            {
+                "id": 3,
+                "name": "Deann Mcclure"
+            },
+            {
+                "id": 4,
+                "name": "Heidi Carson"
+            },
+            {
+                "id": 5,
+                "name": "Rosella Mays"
+            },
+            {
+                "id": 6,
+                "name": "Guerra Griffin"
+            }
+        ]
+    },
+    {
+        "id": 10,
+        "guid": "b719a71a-e632-4002-9e97-9d3455ca75a8",
+        "isActive": false,
+        "balance": 2385,
+        "picture": "//placehold.it/32x32",
+        "age": 36,
+        "name": "Deborah Stewart",
+        "gender": "female",
+        "company": "Flexigen",
+        "email": "deborahstewart@flexigen.com",
+        "phone": "+1 (809) 568-3988",
+        "address": "459 Lincoln Terrace, Datil, New York, 8302",
+        "registered": "2007-07-21T18:09:50 -03:00",
+        "latitude": 28.466815,
+        "longitude": 33.144692,
+        "tags": [
+            "culpa",
+            "incididunt",
+            "cupidatat",
+            "occaecat",
+            "pariatur",
+            "nisi",
+            "voluptate"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Nita Todd"
+            },
+            {
+                "id": 1,
+                "name": "Frances Duran"
+            },
+            {
+                "id": 2,
+                "name": "Christi Lee"
+            },
+            {
+                "id": 3,
+                "name": "Foreman Lawrence"
+            },
+            {
+                "id": 4,
+                "name": "Pruitt Holloway"
+            },
+            {
+                "id": 5,
+                "name": "Mavis Osborn"
+            },
+            {
+                "id": 6,
+                "name": "Andrews Knapp"
+            }
+        ]
+    }
+    
+]
+
+
+// reference:  https://www.shieldui.com/blogs/bootstrap-dashboard?utm_source=tuicool&utm_medium=referral
